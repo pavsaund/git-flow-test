b0880af40f484bd8bd11fffc45e7b5a1a88f234a
Pavneet Singh Saund <pav.saund@gmail.com>
2013-10-29 22:26:53 +0100

    iterating

--- source/longRunning.js
+++ source/longRunning.js
@@ -1,13 +1,15 @@
 (function(){
-"use strict"
+	"use strict"
 
-//this is a self-executing function that doing...well..nothing! :)
+	//this is a self-executing function that doing...well..nothing! :)
 
-var self = this;
+	var self = this;
 
-this.execute = function(){
-	console.log("LongRun starting");
-
-};
+	this.execute = function(){
+		console.log("LongRun starting");
+		for (var i = 5; i >= 0; i--) {
+			console.log("iteration: " + i);
+		};
+	};
 
 })();
